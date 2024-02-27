ff551a39f28c697a6273aa4a135d3bbd088f6113
Zishan Siddique <zishansiddique80@gmail.com>
2024-02-27 18:41:29 +0545

    model now runs with one single image received as form data with key 'image'

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from PIL import Image
+import numpy as np
+import torch
+from torchvision.transforms.functional import pil_to_tensor
 
+from load_model import auto_transforms
+from model import output_from_model
+
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app, origins="*")
 
 
+def preprocess(img):
+    img_as_tensor = pil_to_tensor(img)
+    transformed_img = auto_transforms(img_as_tensor)
+    unsqueezed_img = torch.unsqueeze(transformed_img, 0)
+    return unsqueezed_img
+
+
 @app.route("/", methods=["POST"])
 def predict():
-    input = request.get_json()
-    output = input
+    input = request.files['image']
+    img = Image.open(input.stream)
 
-    return jsonify(output)
+    preprocessed_img = preprocess(img)
+    output = output_from_model(preprocessed_img)
+    return jsonify({'msg': 'success', 'id': int(torch.argmax(output))})
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,11 +1,11 @@
+from load_model import model
 import torch
-import torchvision
-from torch import nn
-from torchvision import transforms
 
 
 # check the device
-device='cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Device: {device}')
 
-weights=torc
+
+def output_from_model(input):
+    return model(input)
