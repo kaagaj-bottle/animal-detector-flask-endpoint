ac73f1d78f1520084a1864393809a044e46332a1
Zishan Siddique <zishansiddique80@gmail.com>
2024-03-05 11:13:33 +0545

    new model constructor for both audio and video created

--- load_model.py
+++ load_model.py
@@ -10,14 +10,29 @@ print(f'Device: {device}')
 auto_transforms = torchvision.models.EfficientNet_B0_Weights.DEFAULT.transforms()
 
 
-# construct the model
-model = efficientnet_b0(weights=None).to(device)
-output_shape = 55
-model.classifier = torch.nn.Sequential(
-    torch.nn.Dropout(p=0.2, inplace=True),
-    torch.nn.Linear(in_features=1280,
-                    out_features=output_shape,
-                    bias=True)).to(device)
+# construct the vision model
+def vision_model_constructor(output_shape: int = 55):
+
+    vision_model = efficientnet_b0(weights=None).to(device)
+    vision_model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2, inplace=True),
+        torch.nn.Linear(in_features=1280,
+                        out_features=output_shape,
+                        bias=True)).to(device)
+
+    vision_model.load_state_dict(torch.load(
+        "models_pth/model_march_3.pth", map_location=device))
+    return vision_model
 
 
-model.load_state_dict(torch.load("models_pth/model_march_3.pth", map_location=device))
+def audio_model_constructor(output_shape: int = 23):
+    audio_model = efficientnet_b0(weights=None).to(device)
+    audio_model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2, inplace=True),
+        torch.nn.Linear(in_features=1280,
+                        out_features=output_shape,
+                        bias=True)).to(device)
+
+    audio_model.load_state_dict(torch.load(
+        "models_pth/model_march_3.pth", map_location=device))
+    return audio_model
